bcdc6f05c418e49e2f0f44c0b153b27d54ad37e3
tarakkrsihna <tarakkrishna311@gmail.com>
2025-07-29 19:05:15 +0530

    29-7-2025

---
+++ Strings/Q434.py
@@ -0,0 +1,4 @@
+class Solution(object):
+    def countSegments(self, s):
+        s=s.split()
+        return len(s)
--- rough1.py
+++ rough1.py
@@ -1,5 +1,14 @@
-s = "AeC"
-if 'a' == 'a':
-    print(True)
-else:
-    print(False)
+import re
+paragraph = "Bob hit a ball, the hit BALL flew far after it was hit."
+banned = ["hit"]
+paragraph=paragraph.lower().split()
+paragraph = re.sub(r'[^\w\s]', '', paragraph)
+print(paragraph)
+# count=0
+# word=''
+# for i in paragraph:
+#     if i not in banned:
+#         if count<paragraph.count(i):
+#             count=paragraph.count(i)
+#             word=i
+# print(word)
