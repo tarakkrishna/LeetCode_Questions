0061372aba4f8aabb30eb28d154252b7230ecb41
tarakkrsihna <tarakkrishna311@gmail.com>
2025-08-04 23:01:03 +0530

    04-08-2025

--- rough.py
+++ rough.py
@@ -1,16 +1,3 @@
-s = "abc1111"
-x = []
-m1, m2 = float('-inf'), float('-inf')
-for i in s:
-    if i.isdigit():
-        x.append(int(i))
-for i in x:
-    if i > m1:
-        m2 = m1
-        m1 = i
-    elif i > m2:
-        m2 = i
-if m2 == float('-inf'):
-    print(-1)
-else:
-    print(m2)
+s='tarakk'
+x=len(s)/2+1
+print(x)
